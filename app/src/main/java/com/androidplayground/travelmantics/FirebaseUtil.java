7172bc575d82d82e402e2eaaa54875e684491ed6
Perpetua-Kabute <kabuteperpetua@gmail.com>
2019-08-08 02:43:21 +0300

    Differentiate user access

--- app/src/main/java/com/androidplayground/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/androidplayground/travelmantics/FirebaseUtil.java
@@ -1,12 +1,17 @@
 package com.androidplayground.travelmantics;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FirebaseUtil {
+public class  FirebaseUtil {
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
     private static FirebaseUtil firebaseUtil;
@@ -22,11 +27,12 @@ public class FirebaseUtil {
     public  static FirebaseAuth.AuthStateListener mAuthListener;
     public static ArrayList<TravelDeal> mDeals;
     private static final int RC_SIGN_IN = 123;
-    public static Activity caller;
+    public static ListActivity caller;
+    public static boolean isAdmin;
 
     private FirebaseUtil(){}
 
-    public static void openFbReference (String ref, final Activity callerActivity){
+    public static void openFbReference (String ref, final ListActivity callerActivity){
         if(firebaseUtil == null){
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
@@ -35,8 +41,14 @@ public class FirebaseUtil {
             mAuthListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                    // Choose authentication providers
-                    FirebaseUtil.signIn();
+                    if(firebaseAuth.getCurrentUser() == null){
+                        FirebaseUtil.signIn();
+                    }
+                    else{
+                        String userId = firebaseAuth.getUid();
+                        checkAdmin(userId);
+                    }
+
                     Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG ).show();
                 }
             };
@@ -46,6 +58,41 @@ public class FirebaseUtil {
         mDeals = new ArrayList<TravelDeal>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
     }
+
+    private static void checkAdmin(String uid) {
+        FirebaseUtil.isAdmin=false;
+        DatabaseReference ref = mFirebaseDatabase.getReference().child("administrators")
+                .child(uid);
+        ChildEventListener listener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                FirebaseUtil.isAdmin=true;
+                caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
+    }
+
     private static void signIn(){
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
@@ -56,6 +103,7 @@ public class FirebaseUtil {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
                         .build(),
                 RC_SIGN_IN);
     }
